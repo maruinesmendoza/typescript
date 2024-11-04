92f9f1697217264194ac7d2efb9461d74a0a8cf8
Sol Lopez <sol.lopez@gruposancristobal.com>
2024-11-04 19:39:04 -0300

    ts exercises

---
+++ typescript.ts
@@ -0,0 +1,50 @@
+// Typescript playground: https://www.typescriptlang.org/play 
+// console.log(): https://developer.mozilla.org/en-US/docs/Web/API/console/log_static 
+// forEach: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+// filter(): https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/filter 
+// Más práctica online: https://www.w3schools.com/typescript/exercise.php?filename=exercise_intro1 
+
+
+
+// Ejercicio 1
+// Declarar una variable de tipo string llamada "nombre" y asignarle tu nombre.
+// Declarar una variable de tipo number llamada "edad" y asignarle tu edad.
+// Declarar una variable de tipo boolean llamada "esEstudiante" y asignarle true o false.
+// Imprimir en consola el valor de cada variable usando console.log()
+
+
+
+// Ejercicio 2
+// Declarar una función que reciba dos números como parámetros y retorne su suma.
+// Asegurar que los parámetros y el valor de retorno sean de tipo number.
+// Llamar a la función e imprimir el resultado.
+// Realizar otra función que reste dos números.
+// Realizar otra función que multiplique dos números.
+
+
+
+// Ejercicio 3
+// Crear un array de números llamado "numeros" y agregar algunos valores.
+// Usa un bucle para imprimir cada número multiplicado por 2 (usando forEach())
+
+
+
+// Ejercicio 4
+// Declarar una interfaz "Persona" con las propiedades: nombre (string), edad (number), y esEstudiante (boolean).
+// Crear tres objetos que sigan la estructura de la interfaz.
+// Imprimir en consola.
+
+
+
+// Ejercicio 5
+// Declara una variable que pueda ser de tipo string o number.
+// Asigna un valor string y luego cambia su valor a number.
+// Imprimir en consola ambos casos.
+
+
+
+// Ejercicio 6
+// Definir una interfaz "Producto" con propiedades: nombre (string), precio (number), y enStock (boolean).
+// Crear un array de "productos" con seis objetos que sigan la interfaz.
+// Crear una función que reciba el array y retorne los nombres de los productos en stock (sólo en stock). 
+// Se puede usar método filter() para esto.
