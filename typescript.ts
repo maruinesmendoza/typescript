8f3f7b00ae559e7bdad4d5fab420128c181dc1ab
Sol Lopez <ingsollopez@gmail.com>
2024-11-13 11:44:21 -0300

    exercises

--- typescript.ts
+++ typescript.ts
@@ -2,6 +2,8 @@
 // console.log(): https://developer.mozilla.org/en-US/docs/Web/API/console/log_static 
 // forEach: https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
 // filter(): https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/filter 
+// map(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map 
+// find(): https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find 
 // Más práctica online: https://www.w3schools.com/typescript/exercise.php?filename=exercise_intro1 
 
 
@@ -48,7 +50,7 @@
 // Crear un array de "productos" con seis objetos que sigan la interfaz.
 // Crear una función que reciba el array y retorne los productos en stock (sólo en stock). 
 // Crear otra función que reciba el array y retorne los productos sin stock (sólo sin stock). 
-// Se puede usar método filter() para esto.
+// Se puede usar método filter() para esto. https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/filter 
 
 
 // Ejercicio 7
@@ -56,4 +58,48 @@
 // Crear un array de "clientes" con seis objetos que sigan la interfaz.
 // Crear una función que reciba el array y retorne los clientes que cumplan una condición (segun el booleano que hayamos creado). 
 // Crear otra función que reciba el array y retorne los clientes que cumplan la condición inversa al punto de arriba (mismo booleano que hayamos elegido). 
-// Se puede usar método filter() para esto.
+// Se puede usar método filter() para esto. https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/filter 
+
+/*
+
+
+Ejercicio 8: Catálogo de Productos - forEach
+Crear un array llamado catalogo con varios objetos de productos. Cada producto debe tener las propiedades nombre (string) y precio (number).
+Usar forEach para imprimir el nombre y el precio de cada producto. https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach 
+
+
+Ejercicio 9: Catálogo de Productos - filter
+Utilizar filter para crear un nuevo array llamado productosBaratos que solo contenga los productos con precio menor a 50. https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/filter 
+Imprimor productosBaratos en la consola.
+
+
+Ejercicio 10: Actualización de Inventario - map
+Utilizar map para crear un nuevo array catalogoConDescuento, donde cada producto tenga un 10% menos de precio. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map 
+Imprimir catalogoConDescuento en la consola.
+
+
+Ejercicio 11: Búsqueda de Usuario - find
+Crear un array llamado usuarios con varios objetos de usuario. Cada usuario debe tener id (number), nombre (string) y activo (boolean).
+Usar find para buscar el usuario con el id 3. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find 
+Imprimir en la consola el objeto del usuario encontrado.
+
+
+Ejercicio 12: Contador de Usuarios Activos - filter y length
+Utilizar filter para crear un nuevo array usuariosActivos que contenga solo los usuarios con activo: true.
+Usar la propiedad length para contar cuántos usuarios activos hay y muestra el resultado en la consola. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/length 
+
+
+Ejercicio 13: Actualización de Estado de Usuarios
+Usando un bucle forEach, cambiar la propiedad activo de cada usuario a false.
+Imprimir el array usuarios para verificar que todos los usuarios están inactivos.
+
+
+Ejercicio 14: Formateo de Productos para Mostrar - map
+Usar el array catalogo.
+Utilizar map para crear un nuevo array productosFormato que contenga el nombre y precio de cada producto como un string en el formato "Producto: [nombre], Precio: $[precio]".
+Para esto podemos usar template strings `string text ${expression} string text`: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals 
+Imprimir el array productosFormato en la consola.
+
+
+
+*/
